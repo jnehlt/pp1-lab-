eb2c6a0f5a24f5945d7255ef84dff18453df5a61
sebastianbakala <cubisticshock@gmail.com>
2017-01-13 10:43:37 +0100

    Add files via upload

---
+++ qsortuse.cpp
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void){
+	
+///	T tab[n];
+///	qsort(tab , n , sizeof (T) , COMP)  T - typ danej
+/// 	COMP - callback - adres funkcji okreœlaj¹cej czy rosn¹co czy malej¹co
+/*	   int COMP(const void *a , const void *b)
+		{
+			const T *p = (T*)a , *q = (T*)b;
+			return *p - *q;
+		}
+*/
+
+	return 0;	
+}
