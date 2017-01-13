e48af446a54bf593cd15831db0d9c9920a6cd8db
sebastianbakala <cubisticshock@gmail.com>
2017-01-13 10:44:45 +0100

    Update qsortuse.cpp

--- qsortuse.cpp
+++ qsortuse.cpp
@@ -4,10 +4,10 @@
 
 int main(void){
 	
-///	T tab[n];
-///	qsort(tab , n , sizeof (T) , COMP)  T - typ danej
-/// 	COMP - callback - adres funkcji okreœlaj¹cej czy rosn¹co czy malej¹co
-/*	   int COMP(const void *a , const void *b)
+/*	T tab[n];
+	qsort(tab , n , sizeof (T) , COMP)  T - typ danej
+ 	COMP - callback - funkcja porÃ³wnujÄ…ca
+	   int COMP(const void *a , const void *b)
 		{
 			const T *p = (T*)a , *q = (T*)b;
 			return *p - *q;
